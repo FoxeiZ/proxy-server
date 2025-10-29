d65cdec7a07851eb0683a8fe389b6134d338add3
FoxeiZ <57582539+FoxeiZ@users.noreply.github.com>
2025-10-29 18:09:34 +0700

    fix: dont make new logger on same name

--- proxy/utils/logger.py
+++ proxy/utils/logger.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Type
 
 from ..config import Config
 
@@ -8,13 +9,17 @@ __all__ = ("get_logger",)
 def get_logger(
     name: str,
     level: str | int = Config.log_level,
-    handler: type[logging.Handler] = logging.StreamHandler,
+    handler: Type[logging.Handler] = logging.StreamHandler,
     formatter: str = "%(asctime)s - %(levelname)s - %(name)s - %(message)s",
 ) -> logging.Logger:
+    logger = logging.getLogger(name)
+    if logger.handlers:
+        return logger
+
     init_handler = handler()
     init_handler.setFormatter(logging.Formatter(formatter))
 
-    logger = logging.getLogger(name)
     logger.setLevel("DEBUG" if Config.debug else level or "INFO")
     logger.addHandler(init_handler)
+    logger.propagate = False
     return logger
